fc1d968615bb26315011d26fbaa8ac0944ef5fdf
Artyom Chumachenko <artyomchumachenko@mail.ru>
2020-12-06 14:30:06 +0300

    strcpy perepolnenie

--- head.h
+++ head.h
@@ -2,18 +2,26 @@
 #define LOT_STUDENTS 5
 #define MAX_SIZE 256
 #define MAX_NUMBER_LESSONS 3
+#define MAX_SCORES 5
 
 typedef struct
 {
     char surName[SIZE_THIRTY];
-    char lesson[SIZE_THIRTY];
+    // char lesson[SIZE_THIRTY];
     char Student[SIZE_THIRTY];
-    int score[SIZE_THIRTY][SIZE_THIRTY];
-} StudentStuct[SIZE_THIRTY];
-StudentStuct Students;
+    // int score[SIZE_THIRTY][SIZE_THIRTY];
+} StudentStuct;
+StudentStuct Students[SIZE_THIRTY];
 
-int menuNumber=0, errorCode=0, NumberStudents=0, NumberLessons=0, studentNumber = 1, numberOfStudents=0;
-char TargetFile[MAX_SIZE];
+Lesson[SIZE_THIRTY][SIZE_THIRTY];
+Score[SIZE_THIRTY][SIZE_THIRTY][SIZE_THIRTY];
+
+int studentNumber[SIZE_THIRTY];
+int currStudent = 0;
+
+int menuNumber = 0, errorCode = 0, NumberStudents = 0, NumberLessons = 0, numberOfStudents = 0, numberLesson = 0;
+int scoreNumber = 0, numberScores = 0, selectNumber = 0;
+char TargetFile[MAX_SIZE] = "";
 FILE *NameFile;
 
 void enterTargetFile()
@@ -29,6 +37,7 @@ void enterTargetFile()
         {
         case 1:
             printf("Задайте путь к рабочему файлу: ");
+            fflush(stdin);
             scanf("%s", TargetFile);
             fflush(stdin);
             NameFile = fopen(TargetFile,"r");
@@ -103,32 +112,17 @@ void clearArray(char *array, int array_size)
         array[i]='\0';
     }
 }
-void enterLessons()
-{
-//    NumberLessons = 0;
-//    errorCode = 0;
-    do
-    {
-        printf("Введите количество добавляемых предметов: ");
-        errorCode = transfer(&NumberLessons);
-//        printf("\n%d    %d\n", errorCode, NumberLessons);
-    }while((errorCode == 1)||(NumberLessons<1)||(NumberLessons>MAX_NUMBER_LESSONS));
-//    fflush(stdin);
-    for(int i=0; i<NumberLessons; i++)
-    {
-        do
-        {
-            printf("Введите название %d предмета: ", i+1);
-            enterArray(Students[i].lesson);
-        }while(checkArrayName(Students[i].lesson)==0);
-    }
-}
 int transfer(int *teleport)
 {
     char buffer[MAX_SIZE];
     fgets(buffer, MAX_SIZE, stdin);
     buffer[strlen(buffer)-1]='\0';
     int k = strlen(buffer);
+    if(k=0)
+    {
+        printf("Ошибка\n");
+        return 1;
+    }
     for(int i=0; i<k; i++)
     {
         if(!isdigit(buffer[i]))
@@ -154,5 +148,51 @@ void printArray(char* array)    //вывод массива
 }
 void readDataBase()
 {
-    
+    char buffer[MAX_SIZE] = "";
+    char *flag = NULL;
+    FILE *NameFile = NULL;
+    NameFile = fopen(TargetFile, "r");
+    while(!feof(NameFile))
+    {
+        flag = fgets(buffer, MAX_SIZE, NameFile);
+        if (buffer[strlen(buffer) - 1] == '\n')
+        {
+            buffer[strlen(buffer) - 1] = '\0';
+        }
+        if(flag != NULL)
+        {
+            char *bufferFile;
+            if(isdigit(buffer[0]))
+            {
+                numberLesson = 0;
+                bufferFile = strtok(buffer, ".");   // когда была точка пробел фамилию записывал как " Чумаченко"
+                studentNumber[currStudent] = atoi(bufferFile);
+                bufferFile = strtok(NULL, "\t");
+                strcpy(Students[currStudent].surName, bufferFile);
+                currStudent++;
+            }
+            else
+            {
+                bufferFile = strtok(buffer, ":");
+                // strcpy(Lesson[currStudent-1][numberLesson], bufferFile);
+
+                // bufferFile = strtok(NULL, " ");
+                // while(bufferFile!=NULL)
+                // {
+                //     Score[currStudent-1][numberLesson][scoreNumber] = atoi(bufferFile);
+                //     bufferFile = strtok(NULL, " ");
+                //     scoreNumber++;
+                // }
+                // do
+                // {
+                //     bufferFile = strtok(NULL, " ");
+                //     Score[currStudent-1][numberLesson][scoreNumber] = atoi(bufferFile);
+                //     scoreNumber++;
+                // }while(bufferFile != NULL);
+
+                numberLesson++;
+            }
+        }
+    }   
+    fclose(NameFile); 
 }
--- main.c
+++ main.c
@@ -14,84 +14,97 @@ int main()
 	for(int i=0; i<SIZE_THIRTY; i++)
     {
         clearArray(Students[i].surName, SIZE_THIRTY);
-        clearArray(Students[i].lesson, SIZE_THIRTY);
         clearArray(Students[i].Student, SIZE_THIRTY);
     }
-    enterTargetFile();
-    enterLessons();
-    do
+    for(int i=0; i<SIZE_THIRTY; i++)
     {
-        do
+        studentNumber[i]=0;
+        for(int j=0; j<SIZE_THIRTY; j++)
         {
-            printf("\nМеню:\n(1) - добавить студента\n(2) - добавить оценки\n(3) - вывести содержимое txt файла\n(4) - очистить txt файл\n(5) - выбрать другой txt файл\n(0) - завершить работу с программой\n\n");
-            errorCode = transfer(&menuNumber);
-        }while(errorCode == 1);
-        switch(menuNumber)
-        {
-        case 1:
+            Lesson[i][j]=0;
+            for(int k=0; k<SIZE_THIRTY; k++)
             {
-                char bufferNumber[MAX_SIZE];
-                do
-                {
-                    printf("Введите количество студентов, которых хотите добавить: ");
-                    errorCode = transfer(&NumberStudents);
-                }while((errorCode == 1)||(NumberStudents<1)||(NumberStudents>LOT_STUDENTS));
-                for(int i=0; i<NumberStudents; i++)
-                {
-                    do
-                    {
-                        printf("Введите фамилию %d студента: ", studentNumber);
-                        enterArray(Students[i].surName);
-                    }while(checkArrayName(Students[i].surName)==0);
-                    sprintf(bufferNumber, "%d", studentNumber);
-                    strcat(Students[i].Student, bufferNumber);
-                    strcat(Students[i].Student, ". ");
-                    strcat(Students[i].Student, Students[i].surName);
-                    studentNumber++;
-                    NameFile = fopen(TargetFile,"a");
-                    fprintf(NameFile,Students[i].Student);
-                    fprintf(NameFile,"\n");
-                    fclose(NameFile);
-                }
-                break;
-            }
-        case 2:
-
-        case 3:
-            {
-                printf("\nСписок студентов:\n");
-                char tempString[MAX_SIZE];
-                NameFile = fopen(TargetFile, "r");
-                while(!feof(NameFile))
-                {
-                    fgets(tempString, MAX_SIZE, NameFile);
-                    if(!feof(NameFile))
-                    {
-                        printArray(tempString);
-                    }
-                }
-                fclose(NameFile);
-                break;
-            }
-        case 4:
-            {
-                NameFile = fopen(TargetFile, "w");
-                fclose(NameFile);
-                break;
-            }
-        case 5:
-            {
-                enterTargetFile();
-                break;
-            }
-        case 0:
-            return 0;
-        default:
-            {
-                printf("Ошибка выбора пункта меню\n");
-                break;
+                Score[i][j][k]=0;
             }
         }
-    }while(menuNumber!=0);
+    }
+    enterTargetFile();
+    readDataBase();
+    printf("%s", Students[0].surName);
+    // do
+    // {
+    //     do
+    //     {
+    //         printf("\nМеню:\n(1) - добавить студента\n(2) - добавить оценки\n(3) - вывести содержимое txt файла\n(4) - очистить txt файл\n(5) - выбрать другой txt файл\n(0) - завершить работу с программой\n\n");
+    //         errorCode = transfer(&menuNumber);
+    //     }while(errorCode == 1);
+    //     switch(menuNumber)
+    //     {
+    //     case 1:
+    //         {
+    //             char bufferNumber[MAX_SIZE];
+    //             do
+    //             {
+    //                 printf("Введите количество студентов, которых хотите добавить: ");
+    //                 errorCode = transfer(&NumberStudents);
+    //             }while((errorCode == 1)||(NumberStudents<1)||(NumberStudents>LOT_STUDENTS));
+    //             for(int i=0; i<NumberStudents; i++)
+    //             {
+    //                 do
+    //                 {
+    //                     printf("Введите фамилию %d студента: ", (studentNumber+1));
+    //                     enterArray(Students[studentNumber].surName);
+    //                 }while(checkArrayName(Students[studentNumber].surName)==0);
+    //                 sprintf(bufferNumber, "%d", (studentNumber+1)); // ????
+    //                 strcat(Students[studentNumber].Student, bufferNumber);
+    //                 strcat(Students[studentNumber].Student, ". ");
+    //                 strcat(Students[studentNumber].Student, Students[studentNumber].surName);
+    //                 strcat(Students[studentNumber].Student, "\t");
+    //                 NameFile = fopen(TargetFile,"a");
+    //                 fprintf(NameFile,Students[studentNumber].Student);
+    //                 fprintf(NameFile,"\n");
+    //                 fclose(NameFile);
+    //                 studentNumber++;
+    //             }
+    //             break;
+    //         }
+    //     case 2:
+    //         printf("%d %s %s", studentNumber, Students[0].surName, Lesson[0][0]);
+    //     case 3:
+    //         {
+    //             printf("\nСписок студентов:\n");
+    //             char tempString[MAX_SIZE];
+    //             NameFile = fopen(TargetFile, "r");
+    //             while(!feof(NameFile))
+    //             {
+    //                 fgets(tempString, MAX_SIZE, NameFile);
+    //                 if(!feof(NameFile))
+    //                 {
+    //                     printArray(tempString);
+    //                 }
+    //             }
+    //             fclose(NameFile);
+    //             break;
+    //         }
+    //     case 4:
+    //         {
+    //             NameFile = fopen(TargetFile, "w");
+    //             fclose(NameFile);
+    //             break;
+    //         }
+    //     case 5:
+    //         {
+    //             enterTargetFile();
+    //             break;
+    //         }
+    //     case 0:
+    //         return 0;
+    //     default:
+    //         {
+    //             printf("Ошибка выбора пункта меню\n");
+    //             break;
+    //         }
+    //     }
+    // }while(menuNumber!=0);
     return 0;
 }
